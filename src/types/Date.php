aa8a876356e6e477c062fa9e3662d41f570b0392
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2024-03-10 17:04:32 -0700

    Minor comment adjustment

--- src/types/Date.php
+++ src/types/Date.php
@@ -4,9 +4,12 @@ namespace Hiraeth\Turso\Types;
 
 use DateTime;
 
+/**
+ * Handles dates
+ */
 class Date {
 	/**
-	 *
+	 * Convert a value from the database to the entity
 	 */
 	static public function from(string|null $date): DateTime|null
 	{
@@ -15,7 +18,7 @@ class Date {
 
 
 	/**
-	 *
+	 * Convert a value from an entity to the database
 	 */
 	static public function to(DateTime|null $date): string|null
 	{
